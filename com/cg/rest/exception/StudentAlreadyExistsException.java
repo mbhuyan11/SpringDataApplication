90f2dca451b407cf23a4a26235d7fa1e5214f989
Mayur Bhuyan <mayurbhuyan4@gmail.com>
2021-09-20 12:12:44 +0530

    SpringDataApplication

---
+++ com/cg/rest/exception/StudentAlreadyExistsException.java
@@ -0,0 +1,9 @@
+package com.cg.rest.exception;
+
+public class StudentAlreadyExistsException extends RuntimeException {
+
+	public StudentAlreadyExistsException(String message) {
+		super(message);
+	}
+
+}
